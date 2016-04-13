9b0ddd2bb5c6e9094b70bedf4e73038a15d0f1a0
Jonathan <Jonathan@DESKTOP-41K9AT3>
2016-04-13 23:30:35 +0200

    Menu angefangen

--- gModsAndroid/gModsAndroid/MainActivity.cs
+++ gModsAndroid/gModsAndroid/MainActivity.cs
@@ -8,7 +8,7 @@ using Android.OS;
 
 namespace gModsAndroid
 {
-    [Activity(Label = "gModsAndroid", MainLauncher = true, Icon = "@drawable/icon")]
+    [Activity(Label = "gModsAndroid", MainLauncher = true, Icon = "@drawable/icon", Theme="@style/gMods")]
     public class MainActivity : Activity
     {
         int count = 1;
@@ -20,11 +20,6 @@ namespace gModsAndroid
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
-            // Get our button from the layout resource,
-            // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.MyButton);
-
-            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
         }
     }
 }
